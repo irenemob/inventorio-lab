ca026043a13f0f7557a8058ac0115c60542a1dcd
irenemob <irenemothio@eq.ufrj.br>
2026-05-28 14:14:07 -0300

    criacao do repositorio e insercao de dados

---
+++ inventario_lab.py
@@ -0,0 +1,19 @@
+# dados do inventario físico 
+# Dados do inventário físico (cada índice representa um frasco individual)
+reagentes = ['Etanol', 'Acetona', 'Etanol', 'Ácido Sulfúrico', 'Benzeno', 'Acetona',
+             'Etanol', 'Ácido Sulfúrico', 'Metanol', 'Tolueno', 'Etanol', 'Acetona',
+             'Ácido Acético', 'Etanol', 'Benzeno', 'Ácido Sulfúrico', 'Metanol',
+             'Ácido Acético', 'Etanol', 'Acetona', 'Tolueno', 'Ácido Sulfúrico',
+             'Benzeno', 'Etanol', 'Acetona', 'Metanol', 'Ácido Sulfúrico', 'Acetona',
+             'Ácido Acético', 'Etanol']
+
+lotes = ['2023-ETA-01', '2023-ACE-01', '2023-ETA-01', '2023-SUL-01', '2023-BEN-01',
+         '2024-ACE-01', '2023-ETA-02', '2024-SUL-01', '2023-MET-01', '2024-TOL-01',
+         '2023-ETA-01', '2023-ACE-01', '2023-ACA-01', '2023-ETA-02', '2023-BEN-01',
+         '2023-SUL-01', '2023-MET-01', '2024-ACA-01', '2023-ETA-01', '2023-ACE-01',
+         '2024-TOL-01', '2024-SUL-01', '2023-BEN-01', '2023-ETA-01', '2023-ACE-01',
+         '2023-MET-01', '2023-SUL-01', '2024-ACE-01', '2024-ACA-01', '2023-ETA-02']
+
+purezas = [99.5, 92.0, 99.5, 98.0, 99.9, 98.5, 96.0, 99.0, 99.0, 98.8, 99.5, 92.0,
+           99.2, 96.0, 99.9, 98.0, 99.0, 95.0, 99.5, 92.0, 98.8, 99.0, 99.9, 99.5,
+           92.0, 99.0, 98.0, 98.5, 95.0, 96.0]
